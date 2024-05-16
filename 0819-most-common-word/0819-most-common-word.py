4ceebe29f187cd414b8c06d730b23974a5bd5bff
Siyeon Won <96099990+1unasy@users.noreply.github.com>
2024-05-16 14:42:35 +0900

    Time: 36 ms (76.29%), Space: 16.6 MB (74.54%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -1,7 +1,6 @@
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         filtered_words = [word for word in re.sub('[^a-z0-9]', ' ', paragraph.lower()).split() if word not in banned]
-        from collections import defaultdict
         dict = defaultdict(int)
         for word in filtered_words:
             dict[word] += 1    
